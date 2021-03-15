e6cc7c7d96f4e1cfae8461ee59d20a8439c31e58
Santiago J. Valls <santiagovalls@gmail.com>
2021-03-15 15:35:21 -0300

    Add pom.xml && App classes

---
+++ src/main/java/ar/com/virtec/App.java
@@ -0,0 +1,13 @@
+package ar.com.virtec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
